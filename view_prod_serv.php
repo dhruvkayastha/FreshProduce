1fed74c353580601feab6620118ee65216323e82
Arpit Jain <arpitkjain@gmail.com>
2019-03-31 23:33:56 +0530

    Refactoring

--- view_prod_serv.php
+++ view_prod_serv.php
@@ -1,16 +1,13 @@
-<?php 
+<?php
 require_once 'dbconnect.php';
-$id = $_COOKIE["user_id"]; 
+
+$id = $_COOKIE["user_id"];
+
 $queryProd = "SELECT product_id, product_name, cost, name, phone_no FROM product JOIN user USING(user_id)";
 $queryServ = "SELECT service_id, service_name, tier, cost, name, phone_no, description FROM service JOIN user USING(user_id)";
 
-
 $resultProd = mysqli_query($con, $queryProd);
 $resultServ = mysqli_query($con, $queryServ);
-
-
-$con->close();
-// $row=mysqli_fetch_array($result);
 
 echo "Products<br><br>";
 while($row = mysqli_fetch_assoc($resultProd)){
@@ -19,9 +16,9 @@ while($row = mysqli_fetch_assoc($resultProd)){
     }
     echo "<br>";
 }
-echo "<br><br>Services<br><br>";
+echo "<br><br>"
 
-
+echo "Services<br><br>";
 while($row = mysqli_fetch_assoc($resultServ)){
     foreach($row as $cname => $cvalue){
         echo "$cvalue\t";
@@ -29,12 +26,13 @@ while($row = mysqli_fetch_assoc($resultServ)){
     echo "<br>";
 }
 echo "<br><br>";
+
 ?>
 
 <script type="text/javascript">
 	var xmlhttp = new XMLHttpRequest();
-		
-	xmlhttp.onreadystatechange = function(){
+
+	xmlhttp.onreadystatechange = function() {
 		if(this.readyState==4 && this.status==200) {
 			var str = this.responseText;
 			alert(str);
@@ -51,18 +49,6 @@ echo "<br><br>";
 		xmlhttp.send(data);
 	}
 	function buyServ() {
-		// alert("Ajax");
-		// var xmlhttp = new XMLHttpRequest();
-		
-		// xmlhttp.onreadystatechange = function(){
-		// 	if(this.readyState==4 && this.status==200) {
-		// 		// var obj = JSON.parse(this.responseText);
-		// 		var str = this.responseText;
-		// 		alert(str);
-		// 		document.getElementById("result").innerHTML = str;
-		// 	}
-			
-		// };
 		var serv = document.getElementById("serv_id").value;
 		var qty = document.getElementById("qty2").value;
 		xmlhttp.open("POST", "buy.php", true);
@@ -77,13 +63,19 @@ echo "<br><br>";
 	<title>Products & Services | FreshProduce</title>
 </head>
 <body>
-		<input type="text" name="prod_id" id="prod_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Product ID" required>
-		<input type="text" name="qty1" id="qty1" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
-		<button onclick="buyProd()">Buy Product</button>
+	<input type="text" name="prod_id" id="prod_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Product ID" required>
+	<input type="text" name="qty1" id="qty1" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
+	<button onclick="buyProd()">Buy Product</button>
+
 	<br><br>
-		<input type="text" name="serv_id" id="serv_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Service ID" required>
-		<input type="text" name="qty2" id="qty2" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
-		<button onclick="buyServ()">Buy Product</button>
-	<br><br><p id="result"></p>
+
+	<input type="text" name="serv_id" id="serv_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Service ID" required>
+	<input type="text" name="qty2" id="qty2" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
+	<button onclick="buyServ()">Buy Product</button>
+
+	<br><br>
+
+	<p id="result"></p>
+
 </body>
 </html>
