de1662e2b3382afeea69807438fc9839bd333728
Arpit Jain <arpitkjain@gmail.com>
2019-04-01 23:18:58 +0530

    Add filters

--- view_prod_serv.php
+++ view_prod_serv.php
@@ -1,10 +1,34 @@
+<html>
+<head>
+	<title>Products & Services | FreshProduce</title>
+</head>
+<body>
+
+
 <?php
 require_once 'dbconnect.php';
 
 $id = $_COOKIE["user_id"];
 
-$queryProd = "SELECT product_id, product_name, cost, name, phone_no FROM product JOIN user USING(user_id)";
-$queryServ = "SELECT service_id, service_name, tier, cost, name, phone_no, description FROM service JOIN user USING(user_id)";
+$queryProd = "SELECT product_id, product_name, cost, name, phone_no FROM product JOIN user USING(user_id) WHERE 1=1 ";
+
+foreach ($_GET as $key => $value)
+	{
+		if($key[1]==='1' && $value != "")
+		{
+			$queryProd=$queryProd."AND ".substr($key, 3)."='".$value."' ";
+		}
+	}
+
+$queryServ = "SELECT service_id, service_name, tier, cost, name, phone_no, description FROM service JOIN user USING(user_id) WHERE 1=1 ";
+
+foreach ($_GET as $key => $value)
+	{
+		if($key[1]==='2' && $value != "")
+		{
+			$queryServ=$queryServ."AND ".substr($key, 3)."='".$value."' ";
+		}
+	}
 
 $resultProd = mysqli_query($con, $queryProd);
 $resultServ = mysqli_query($con, $queryServ);
@@ -17,7 +41,23 @@ while($row = mysqli_fetch_assoc($resultProd)){
     echo "<br>";
 }
 echo "<br><br>";
+?>
 
+<form action="?" method="get">
+	<input type="text" name="f1_product_id" id="f1_product_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Product ID">
+	<input type="text" name="f1_product_name" id="f1_product_name" pattern="[A-Za-z0-9 ]{1,}" placeholder="Product Name">
+	<input type="text" name="f1_cost<" id="f1_cost<" pattern="[0-9]{1,8}" placeholder="Cost">
+	<input type="text" name="f1_name" id="f1_name" pattern="[A-Za-z0-9 ]{1,}" placeholder="Seller Name">
+	<button type="submit">Filter Products</button>
+</form>
+<br><br>
+	<input type="text" name="prod_id" id="prod_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Product ID" required>
+	<input type="text" name="qty1" id="qty1" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
+	<button onclick="buyProd()">Buy Product</button>
+
+	<br><br>
+
+<?php
 echo "Services<br><br>";
 while($row = mysqli_fetch_assoc($resultServ)){
     foreach($row as $cname => $cvalue){
@@ -28,6 +68,24 @@ while($row = mysqli_fetch_assoc($resultServ)){
 echo "<br><br>";
 
 ?>
+
+<!-- service_id, service_name, tier, cost, name, phone_no, description -->
+<form action="?" method="get">
+	<input type="text" name="f2_service_id" id="f2_service_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Service ID">
+	<input type="text" name="f2_service_name" id="f2_service_name" pattern="[A-Za-z0-9 ]{1,}" placeholder="Service Name">
+	<input type="text" name="f2_tier" id="f2_tier" pattern="[A-Za-z]{1,}" placeholder="Service Tier">
+	<input type="text" name="f2_cost<" id="f2_cost<" pattern="[0-9]{1,8}" placeholder="Cost">
+	<input type="text" name="f2_name" id="f2_name" pattern="[A-Za-z0-9 ]{1,}" placeholder="Seller Name">
+	<button type="submit">Filter Products</button>
+</form>
+<br><br>
+	<input type="text" name="serv_id" id="serv_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Service ID" required>
+	<input type="text" name="qty2" id="qty2" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
+	<button onclick="buyServ()">Buy Product</button>
+
+	<br><br>
+</body>
+</html>
 
 <script type="text/javascript">
 	var xmlhttp = new XMLHttpRequest();
@@ -79,25 +137,3 @@ echo "<br><br>";
 		xmlhttp.send(data);
 	}
 </script>
-
-<html>
-<head>
-	<title>Products & Services | FreshProduce</title>
-</head>
-<body>
-	<input type="text" name="prod_id" id="prod_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Product ID" required>
-	<input type="text" name="qty1" id="qty1" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
-	<button onclick="buyProd()">Buy Product</button>
-
-	<br><br>
-
-	<input type="text" name="serv_id" id="serv_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Service ID" required>
-	<input type="text" name="qty2" id="qty2" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Quantity" required>
-	<button onclick="buyServ()">Buy Product</button>
-
-	<br><br>
-
-	<p id="result"></p>
-
-</body>
-</html>
--- view_requirements.php
+++ view_requirements.php
@@ -1,28 +1,52 @@
-<?php 
-	require_once 'dbconnect.php';
-
-	$query = "SELECT crop_id, crop_name, crop_type, quantity FROM requirements JOIN crops USING(crop_id)";
-
-	$result = mysqli_query($con, $query);
-	if($result!==false)
-	{
-		while($row = mysqli_fetch_assoc($result)){
-	    	foreach($row as $cname => $cvalue){
-	        	print "$cvalue\t";
-	    	}
-	    	print "<br>";
-		}	
-	}
-	else 
-	{
-		print "No requirements in market";
-	}
-?>
 <html>
 <head>
 	<title>Market Requirements | FreshProduce</title>
 </head>
 <body>
+
+
+<?php
+	require_once 'dbconnect.php';
+
+	$query = "SELECT crop_id, crop_name, crop_type, quantity FROM requirements JOIN crops USING(crop_id) WHERE 1=1 ";
+
+	foreach ($_GET as $key => $value)
+		{
+			if($value != "")
+			{
+				$query=$query."AND ".substr($key, 2)."='".$value."' ";
+			}
+		}
+
+	echo "Requirements<br><br>";
+	$result = mysqli_query($con, $query);
+	if($result!==false)
+	{
+		while($row = mysqli_fetch_assoc($result)){
+	    	foreach($row as $cname => $cvalue){
+	        	echo "$cvalue\t";
+	    	}
+	    	echo "<br>";
+		}
+	}
+	else
+	{
+		echo "No specified requirements in market";
+	}
+
+	echo "<br><br>"
+?>
+
+<form action="?" method="get">
+	<input type="text" name="f_crop_id" id="f_crop_id" pattern="[0-9]{1,8}" title="Enter numeric ID" placeholder="Crop ID">
+	<input type="text" name="f_crop_name" id="f_crop_name" pattern="[A-Za-z]{1,}" placeholder="Crop Name">
+	<input type="text" name="f_crop_type" id="f_crop_type" pattern="[A-Za-z]{1,}" placeholder="Crop Type">
+	<input type="text" name="f_quantity>" id="f_quantity>" pattern="[0-9]{1,8}" placeholder="Quantity Lower Bound">
+	<input type="text" name="f_quantity<" id="f_quantity<" pattern="[0-9]{1,8}" placeholder="Upper Bound Quantity">
+	<button type="submit">Filter</button>
+</form>
+<br><br>
+
 	<br><br><br><a href="farmer.php">Click here to go back</a>
 </body>
 </html>
