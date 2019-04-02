f53bf59711c3699dea3ae29988b1b07fc18c6b07
Dhruv Kayastha <dhruvkayastha@gmail.com>
2019-04-02 14:41:22 +0530

    Final

--- signup-backend.php
+++ signup-backend.php
@@ -19,14 +19,30 @@ $numResults = mysqli_num_rows($result);
 
 if($numResults == 1)
 {
-	echo "This email id is already registered.";
-	echo "<a href=\"/FreshProduce/index.php\">Click here to go back</a>";
+	?>
+	<script>
+			alert("This email id is already registered");
+			window.location.replace("index.php");
+
+	</script>
+	<?php
+	// echo "This email id is already registered.";
+	// echo "<a href=\"/FreshProduce/index.php\">Click here to go back</a>";
+	// header("Location: index.php");
 }
 else
 {
 	$stmt->execute();
-	echo "User was registered successfully.";
-	echo "<a href=\"/FreshProduce/index.php\">Click here to login</a>";
+	// echo "User was registered successfully.";
+	// echo "<a href=\"/FreshProduce/index.php\">Click here to login</a>";
+	?>
+	<script type="text/javascript">
+		alert("User was registered successfully.");
+		window.location.replace("index.php");
+
+	</script>
+	<?php
+			// header("Location: index.php");
 }
 ?>
 
